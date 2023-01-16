58f5797d27b975cebcb8859ac0eb582891cc4d2d
Damián <damian@clancig.com.ar>
2023-01-16 17:27:28 -0300

    FirstCommit

---
+++ src/main/java/com/example/TallerJava/TallerJavaTeam2Application.java
@@ -0,0 +1,13 @@
+package com.example.TallerJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerJavaTeam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerJavaTeam2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TallerJava/TallerJavaTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TallerJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerJavaTeam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
